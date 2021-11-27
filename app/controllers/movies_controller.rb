63e5a6b70404c42a764265b4c6ca8844236eddd4
Yeison <enmanuelquiterios@gmail.com>
2021-11-26 23:05:37 -0500

    First commit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,15 +1,27 @@
 class MoviesController < ApplicationController
+  # This way I can use the rescue Error fix
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
     movies = Movie.all
     render json: movies
   end
-
+# How no to include values Using Active record serializer 
   def show
     movie = Movie.find(params[:id])
-    render json: movie
+
+    # First approach
+    render json: movie.to_json(only: [:id, :tile, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director])
+
+    # Second approach render everything except
+    # render json: movie.to_json(except: [:created_at, :updated_at])
+
   end
+  # How to handle nested routes 
+  # def show
+  #   post = Post.find(params[:id])
+  #   render json: post.to_json(only: [:title, :description, :id], include: [author: { only: [:name]}])
+  # end
 
   private
 
